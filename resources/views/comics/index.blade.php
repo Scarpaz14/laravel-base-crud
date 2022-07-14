fff67ed7ec367c75c691aebcd9ae6e2af175bf17
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-14 20:32:05 +0200

    collegato controller alle rotte index e show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,7 +7,33 @@
 @section('page-content')
     <div class="container">
         <h1>Tutti i fumetti:</h1>
-        @dump($comics)
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Immagine</th>
+                <th scope="col">Tipo di fumetto</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Info</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <td>{{$comic['id']}}</td>
+                        <td>{{$comic['title']}}</td>
+                        <td><img src="{{$comic['thumb']}}"></td>
+                        <td>{{$comic['type']}}</td>
+                        <td>{{$comic['price']}}$</td>
+                        <td>
+                            <a class="btn btn-success" href="{{route('comics.show', $comic->id)}}">Visualizza Info</a>
+                        </td>
+                    </tr>
+                @endforeach
+              
+            </tbody>
+          </table>
 
     </div>
     
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.base')
+
+@section('head-title')
+    Comics Info
+@endsection
+
+@section('page-content')
+    <div class="container my-3">
+        <a class="btn btn-success" href="{{route('comics.index', $comic->id)}}">Torna alla Home</a>
+        <h1>Info dei fumetti:</h1>
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Periodo di sconto</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{$comic['id']}}</td>
+                    <td>{{$comic['description']}}</td>
+                    <td>{{$comic['series']}}</td>
+                    <td>{{$comic['sale_date']}}</td>
+                </tr>
+            </tbody>
+          </table>
+
+    </div>
+    
+@endsection
