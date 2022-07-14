d7a4ece5d5f3f2a55fa1fa6c87fe836934921966
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-14 18:45:26 +0200

    creata tabella, model e iniziato seeder

---
+++ database/migrations/2022_07_14_163101_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table-> string('title', 100);
+            $table-> text('description', 100);
+            $table-> string('thumb', 100)->nullable();
+            $table-> float('price', 4,2);
+            $table-> string('series', 50);
+            $table-> string('series', 50);
+            $table-> date('sale_date');
+            $table-> string('type', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $newComic = New Comic();
+
+
+        
+        $newComic->save();
+    }
+}
